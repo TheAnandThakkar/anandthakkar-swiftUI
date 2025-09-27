a163fd0372dae663f09553d6ded8d015781d1fea
Anand Thakkar <anand.thakkar92@gmail.com>
2025-09-27 12:43:02 +0530

    Initial Commit

---
+++ Anand Thakkar/Anand_ThakkarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anand_ThakkarApp.swift
+//  Anand Thakkar
+//
+//  Created by Anand Thakkar on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Anand_ThakkarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
